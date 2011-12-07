0dc285512fc4c47bee830ee4b42c60f66bac84c3
dewski <xhtmlthis@me.com>
2011-12-07 11:31:50 -0800

    add erb files back after using copy_file instead of template in generators

--- lib/generators/kss/install_generator.rb
+++ lib/generators/kss/install_generator.rb
@@ -6,9 +6,9 @@ module Kss
     desc "KSS Rails Install"
 
     def install_steps
-      template 'index.html', 'app/views/kss/home/index.html.erb'
-      template 'styleguide.html', 'app/views/kss/home/styleguide.html.erb'
-      template 'application.html', 'app/views/layouts/kss/application.html.erb'
+      copy_file 'index.html.erb', 'app/views/kss/home/index.html.erb'
+      copy_file 'styleguide.html.erb', 'app/views/kss/home/styleguide.html.erb'
+      copy_file 'application.html.erb', 'app/views/layouts/kss/application.html.erb'
       route "mount Kss::Engine => '/kss' if Rails.env.development?"
     end
   end
