c3b0c227a66066e87769033c3fd1a850198475e4
dewski <xhtmlthis@me.com>
2011-12-07 11:20:22 -0800

    add index.html.erb template

--- lib/generators/kss/install_generator.rb
+++ lib/generators/kss/install_generator.rb
@@ -6,6 +6,7 @@ module Kss
     desc "KSS Rails Install"
 
     def install_steps
+      template 'index.html.erb', 'app/views/kss/home/index.html.erb'
       template 'styleguide.html.erb', 'app/views/kss/home/styleguide.html.erb'
       template 'application.html.erb', 'app/views/layouts/kss/application.html.erb'
       route "mount Kss::Engine => '/kss' if Rails.env.development?"
